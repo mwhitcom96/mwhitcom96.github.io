5ae981fa8f6fe8c635bf33a29ddb9343ca772927
mwhitcom96 <matt_whitcomb@yahoo.com>
2021-04-03 13:37:49 -0400

    new

--- js/java/script.js
+++ js/java/script.js
@@ -49,3 +49,23 @@ $( function() {
   $( function() {
     $( "#tabs" ).tabs();
   } );
+  $( function() {
+    var state = true;
+    $( "#button" ).on( "click", function() {
+      if ( state ) {
+        $( "#effect" ).animate({
+          backgroundColor: "#aa0000",
+          color: "#fff",
+          width: 500
+        }, 1000 );
+      } else {
+        $( "#effect" ).animate({
+          backgroundColor: "#fff",
+          color: "#000",
+          width: 240
+        }, 1000 );
+      }
+      state = !state;
+    });
+  } );
+  </scr
