3ff3c5d1aca5421bcb9d62de107a309edb322380
mwhitcom96 <matt_whitcomb@yahoo.com>
2021-04-03 09:25:47 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 function revealMessage() {
    document.getElementById('hiddenMessage').style.display = 'block';
 }
+
+function countDown() {
+   var currentVal = document.getElementById("countDownButton").innerHTML = newVal;
+}
