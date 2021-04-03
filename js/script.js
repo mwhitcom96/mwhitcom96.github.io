2b45cebd56d0f865c8c956dac503f4594943401e
mwhitcom96 <matt_whitcomb@yahoo.com>
2021-04-03 09:01:04 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function revealMessage() {
 var count =(function () {
    var counter = 0;
    return function () {return counter +=1;}
-})
+})();
 
 function displaycount() {
    document.getElementById("carrier").innerHTML = count();
