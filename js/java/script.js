26737970bd490400784ec638b239005f7935e632
mwhitcom96 <matt_whitcomb@yahoo.com>
2021-04-03 13:09:43 -0400

    new

--- js/java/script.js
+++ js/java/script.js
@@ -21,35 +21,28 @@ function goBack() {
   window.history.back()
 }
 $( function() {
-    // run the currently selected effect
-    function runEffect() {
-      // get effect type from
-      var selectedEffect = $( "#effectTypes" ).val();
+    var spinner = $( "#spinner" ).spinner();
  
-      // Most effect types need no options passed by default
-      var options = {};
-      // some effects have required parameters
-      if ( selectedEffect === "scale" ) {
-        options = { percent: 50 };
-      } else if ( selectedEffect === "size" ) {
-        options = { to: { width: 280, height: 185 } };
+    $( "#disable" ).on( "click", function() {
+      if ( spinner.spinner( "option", "disabled" ) ) {
+        spinner.spinner( "enable" );
+      } else {
+        spinner.spinner( "disable" );
       }
- 
-      // Run the effect
-      $( "#effect" ).show( selectedEffect, options, 500, callback );
-    };
- 
-    //callback function to bring a hidden box back
-    function callback() {
-      setTimeout(function() {
-        $( "#effect:visible" ).removeAttr( "style" ).fadeOut();
-      }, 1000 );
-    };
- 
-    // Set effect from select menu value
-    $( "#button" ).on( "click", function() {
-      runEffect();
+    });
+    $( "#destroy" ).on( "click", function() {
+      if ( spinner.spinner( "instance" ) ) {
+        spinner.spinner( "destroy" );
+      } else {
+        spinner.spinner();
+      }
+    });
+    $( "#getvalue" ).on( "click", function() {
+      alert( spinner.spinner( "value" ) );
+    });
+    $( "#setvalue" ).on( "click", function() {
+      spinner.spinner( "value", 5 );
     });
  
-    $( "#effect" ).hide();
+    $( "button" ).button();
   } );
