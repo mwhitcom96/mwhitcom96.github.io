094611bb4549d3af04d815f29098a97067b0344a
mwhitcom96 <matt_whitcomb@yahoo.com>
2021-04-03 08:54:57 -0400

    new

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 function revealMessage() {
    document.getElementById('hiddenMessage').style.display = 'block';
 }
+
+var count =(function () {
+   var counter = 0;
+   return function () {return counter +=1;}
+})
+
 function displaycount() {
    document.getElementById("carrier").innerHTML = count();
 }
